80ae1eaf6de58335721b333f7ff53d965fddd4e7
kittijaaa <Kittipat.chofaw@gmail.com>
2024-05-08 22:00:50 +0700

    main

---
+++ OurGroup.java
@@ -0,0 +1,19 @@
+import java.util.Vector;
+
+public class OurGroup {
+	private Vector<String> groupMembers;
+	
+	public OurGroup() {
+		groupMembers = new Vector<String>();
+		groupMembers.add("Sasicha Salabkaeo");
+		groupMembers.add("Kittipat Chofa");
+		groupMembers.add("Sukanya Khamcharoen");
+		groupMembers.add("Nared Fuengverojsakul");
+		groupMembers.add("Pachara Phungauypol");
+		groupMembers.add("Alisa Kajangjaem");
+	}
+	
+	public Vector<String> getGroupMemmbers(){
+		return groupMembers;
+	}
+}
---
+++ group8/app/Http/Controllers/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $userID = Auth::id();
+        // dd($userID);
+        if($userID->roleID==1){
+            return view('home');
+        }
+        else{
+            return view('report');
+        }
+    }
+}
